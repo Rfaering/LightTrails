a4dea98a78d89a059d39576e9508ac16e0cafc6c
Rasmus Larsen <rfaering@gmail.com>
2017-06-22 00:43:48 +0200

    Updated layout of add effect

--- LightTrails/Assets/UI/Effects/EffectContentContainer.cs
+++ LightTrails/Assets/UI/Effects/EffectContentContainer.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections;
+﻿using Assets.UI.Models;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class EffectContentContainer : MonoBehaviour
 {
     public GameObject Prefab;
+
+    public Effect.EffectKind EffectKind = Effect.EffectKind.Particle;
 
     void Start()
     {
@@ -13,7 +17,7 @@ public class EffectContentContainer : MonoBehaviour
             DestroyObject(menuItem.gameObject);
         }
 
-        foreach (var effect in EffectOptions.Options)
+        foreach (var effect in EffectOptions.Options.Where(x => x.Type == EffectKind))
         {
             var newGameObject = Instantiate(Prefab, transform);
             newGameObject.name = effect.Name;
